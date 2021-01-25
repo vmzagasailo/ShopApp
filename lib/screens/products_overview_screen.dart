0864b304a6b0814924e6f5b65e4460ff5b6ac5f0
Volodymyr <vmzagasailo@gmail.com>
2021-01-25 19:08:34 +0200

    create nain screen

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop/models/product.dart';
+
+class ProductOverviewScrenn extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
