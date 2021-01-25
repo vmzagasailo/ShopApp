ab38784ce70320f359ddab5938ba755ae8e17d19
Volodymyr <vmzagasailo@gmail.com>
2021-01-25 19:12:32 +0200

    create main screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,11 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
 
 class ProductOverviewScrenn extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold();
   }
 }
