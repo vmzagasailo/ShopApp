b5510433e976b895d14f29fc57c6d2aee804e859
Volodymyr <vmzagasailo@gmail.com>
2021-01-28 14:33:11 +0200

    get detailed product data using provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   static const routeName = 'product-details';
   @override
   Widget build(BuildContext context) {
+    final prodId = ModalRoute.of(context).settings.arguments as String;
+    final productData =
+        Provider.of<ProductsProvider>(context, listen: false).findById(prodId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(productData.title),
       ),
     );
   }
