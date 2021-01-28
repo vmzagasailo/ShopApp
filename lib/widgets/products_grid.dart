88caa989970c5aa3dc935be6ba1edc81e083420c
Volodymyr <vmzagasailo@gmail.com>
2021-01-28 12:25:34 +0200

    receiving data usin provider

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../models/product.dart';
+import '../providers/products_provider.dart';
 import '../widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
-  const ProductsGrid({
-    Key key,
-    @required this.productList,
-  }) : super(key: key);
-
-  final List<Product> productList;
-
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+    final products = productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
-      itemCount: productList.length,
+      itemCount: products.length,
       itemBuilder: (ctx, index) => ProductItem(
-        productList[index].id,
-        productList[index].title,
-        productList[index].imageUrl,
+        products[index].id,
+        products[index].title,
+        products[index].imageUrl,
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
