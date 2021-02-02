56b014ae145a800ef32659bd7c1ff1c3153bfcc6
Volodymyr <vmzagasailo@gmail.com>
2021-02-02 18:14:22 +0200

    added PopupMenu

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,14 +2,49 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductOverviewScrenn extends StatelessWidget {
+enum ProductsFilter {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScrenn extends StatefulWidget {
+  @override
+  _ProductOverviewScrennState createState() => _ProductOverviewScrennState();
+}
+
+class _ProductOverviewScrennState extends State<ProductOverviewScrenn> {
+  var _showOnlyFavorites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('myShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (ProductsFilter selectedValue) {
+              setState(() {
+                if (selectedValue == ProductsFilter.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Show favorites products'),
+                value: ProductsFilter.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show all products'),
+                value: ProductsFilter.All,
+              ),
+            ],
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
