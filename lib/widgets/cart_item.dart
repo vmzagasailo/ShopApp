0ce5cf0043802b4084c782c33f4e6f0cc540ca92
Volodymyr <vmzagasailo@gmail.com>
2021-02-03 11:37:51 +0200

    displaying product information in the cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class CartItem extends StatelessWidget {
+  final String id;
+  final double price;
+  final int quntity;
+  final String title;
+
+  CartItem({
+    this.id,
+    this.price,
+    this.quntity,
+    this.title,
+  });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Card(
+      margin: EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(child: Text('\$$price'))),
+          ),
+          title: Text(title),
+          subtitle: Text('Total: \$${(price * quntity)}'),
+          trailing: Text('$quntity x'),
+        ),
+      ),
     );
   }
 }
