6423f8fa661cb81697c8b6c537fa8246245fa621
Volodymyr <vmzagasailo@gmail.com>
2021-02-04 14:28:44 +0200

    added method for adding order

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -19,8 +19,19 @@ class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
 
   List<OrderItem> get orders {
-    return[..._orders];
+    return [..._orders];
   }
 
- 
+  void addOrder(List<CartItem> cartsProducts, double total) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        dateTime: DateTime.now(),
+        products: cartsProducts,
+      ),
+    );
+    notifyListeners();
+  }
 }
