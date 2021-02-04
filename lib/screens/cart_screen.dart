f8d373ba69f495a39ef1105a5c34eede73718f7f
Volodymyr <vmzagasailo@gmail.com>
2021-02-04 17:21:05 +0200

    fixed total amount display

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -31,7 +31,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      '\$${cartData.totalAmount}',
+                      '\$${cartData.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                           color:
                               Theme.of(context).primaryTextTheme.title.color),
