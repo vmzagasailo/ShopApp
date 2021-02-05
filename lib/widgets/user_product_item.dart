bd7586b579e97205f2b10fdb9376ebf925173db2
Volodymyr <vmzagasailo@gmail.com>
2021-02-05 15:00:22 +0200

    sending products to EditProductScreen

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../screens/edit_product_screen.dart';
+
 class UserProductitem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
-  UserProductitem({this.title, this.imageUrl});
+  UserProductitem({this.id, this.title, this.imageUrl});
   @override
   Widget build(BuildContext context) {
     return ListTile(
@@ -19,7 +22,9 @@ class UserProductitem extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+              },
             ),
             IconButton(
               icon: Icon(Icons.delete),
