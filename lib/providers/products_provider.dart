56138b714445c76f02558abb93d00dcec1b5ec59
Volodymyr <vmzagasailo@gmail.com>
2021-02-05 14:40:16 +0200

    added method for adding custom products

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -50,4 +50,16 @@ class ProductsProvider with ChangeNotifier {
   Product findById(String id) {
     return _items.firstWhere((product) => product.id == id);
   }
+
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+    notifyListeners();
+  }
 }
