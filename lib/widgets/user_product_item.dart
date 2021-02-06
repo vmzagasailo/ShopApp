4e155a86eee3f08b877054fca0ee58d9cc7abaf0
Volodymyr <vmzagasailo@gmail.com>
2021-02-06 21:24:15 +0200

    implemented delete product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 import '../screens/edit_product_screen.dart';
 
@@ -23,13 +26,16 @@ class UserProductitem extends StatelessWidget {
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
               onPressed: () {
-                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
               },
             ),
             IconButton(
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<ProductsProvider>(context, listen: false).deleteProduct(id);
+              },
             ),
           ],
         ),
