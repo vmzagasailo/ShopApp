15c236531c7cd6c9066f1e386297fa9c41c0f120
Volodymyr <vmzagasailo@gmail.com>
2021-02-09 15:43:56 +0200

    created signup method

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,7 +1,24 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AuthProvider with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+
+  Future<void> signup(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB4sEEWDWY13q_ogHrC-hbfe_rJK8xjO8Q';
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+  }
 }
