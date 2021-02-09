75882468ea23db0e2e830358d615bb441abc4bea
Volodymyr <vmzagasailo@gmail.com>
2021-02-09 19:37:16 +0200

    handling errors

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exeption.dart';
+
 class AuthProvider with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -11,17 +13,24 @@ class AuthProvider with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyB4sEEWDWY13q_ogHrC-hbfe_rJK8xjO8Q';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(response.body);
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpExeption(message: responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
