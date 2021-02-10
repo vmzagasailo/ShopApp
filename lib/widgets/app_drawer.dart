1e8b960be48c4145d406809de6b77846ae121735
Volodymyr <vmzagasailo@gmail.com>
2021-02-10 22:10:38 +0200

    added logout button

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth_provider.dart';
 
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
@@ -43,6 +46,18 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+
+            leading: Icon(
+              Icons.exit_to_app,
+            ),
+            title: const Text('Exit'),
+            onTap: () {
+              Navigator.pop(context);
+              Provider.of<AuthProvider>(context, listen: false).logout();
             },
           ),
         ],
