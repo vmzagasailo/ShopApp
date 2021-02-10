afd8a0ac2cec36f8ef2f480fe958a2dc8db482fd
Volodymyr <vmzagasailo@gmail.com>
2021-02-10 22:10:07 +0200

    added logout method

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -67,4 +67,11 @@ class AuthProvider with ChangeNotifier {
   Future<void> signin(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
+  }
 }
